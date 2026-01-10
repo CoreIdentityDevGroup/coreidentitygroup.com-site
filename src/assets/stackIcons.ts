78da40bae9f74f305b5c411671fe7442230b209f
Todd Morgan <info@coreholdingcorp.com>
2026-01-10 12:15:27 -0500

    assets: use transparent SmartNation PNG (stop base64 webp-in-svg)

--- src/assets/stackIcons.ts
+++ src/assets/stackIcons.ts
@@ -2,7 +2,10 @@
 
 import sentinel from "./icons/sentinel.svg";
 import nexus from "./icons/nexus.svg";
-import smartnation from "./icons/smartnation.svg";
+
+// SmartNation is illustration artwork (raster) for fidelity.
+// Do NOT embed base64 webp inside svg; it degrades on mobile.
+import smartnation from "./icons/smartnation.png";
 
 export const stackIcons = {
   sentinel,
